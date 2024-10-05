7b49e6b9279c120a5e9ec02e3bf786796f607927
Albeoris <albeoris@gmail.com>
2024-10-05 05:21:11 +0200

    Remove NotSupportedException Spam

---
+++ Memoria.Bloomtown/Shared/HarmonyHooks/Fixes/LazyInput_GetKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using LazyBearTechnology;
+using Memoria.Bloomtown.BeepInEx;
+
+namespace Memoria.Bloomtown.HarmonyHooks.AlwaysRun;
+
+[HarmonyPatch]
+public static class RemoveNotSupportedExceptionSpam
+{
+    [HarmonyTargetMethods]
+    private static MethodInfo[] GetTargetMethods()
+    {
+        return typeof(UnityEngine.Windows.Input).GetMethods().Where(m => m.Name == "ForwardRawInput").ToArray();
+    }
+    
+    public static Boolean Prefix()
+    {
+        return false; // don't call original method
+    }
+}
